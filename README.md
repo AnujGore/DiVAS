f8b05fbe8341f47a9a15a1dab460ebc4eee61c8c
anujgore <anujgore23@gmail.com>
2024-09-15 21:39:53 +0100

    Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DiVAS
 Python script to automate daily schedule.
+
+
+# To Do:
+ - Add buffer time between events
+ - Add supervised model to determine the importance factor of each task
--- helpers.py
+++ helpers.py
@@ -1,16 +1,6 @@
 import numpy as np
 
-blocking_params = [
-    #To properly block, label these from >10
-    #(Label, duration (15 minute increments), ToD)
-    (10, 3, 6),
-    (11, 4, 7),
-    (12, 2, (12, 19)), 
-    (13, 1, 16)
-]
-    
-
-def block(arr):
+def block(arr, blocking_params):
     """
     Blocks certain parts of the day for tasks.    
     """
@@ -19,11 +9,11 @@ def block(arr):
         if type(item[-1]) == tuple:
             for t in item[-1]:
                 loc_arr = np.where(arr[:, 1] == t)[0][0]
-                arr[loc_arr:loc_arr+item[1], 0] = item[0]
+                arr[loc_arr:loc_arr+item[1]+1, 0] = item[0]
         
         else:
             loc_arr = np.where(arr[:, -1] == item[-1])[0][0]
-            arr[loc_arr:loc_arr+item[1], 0] = item[0]
+            arr[loc_arr:loc_arr+item[1]+1, 0] = item[0]
             
 
     return arr
